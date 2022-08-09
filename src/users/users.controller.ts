d5df93108facf5d1a67e5a910ddb12905fa56afd
genieouz <genieouzog@gmail.com>
2022-08-09 21:39:52 +0000

    auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '~/auth/auth.guard';
+import { CurrentUser } from '~/auth/decorators/current-user.decorator';
+import { IUser } from './interfaces/user.interface';
+import { UsersService } from './users.service';
 
+@UseGuards(AuthGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    fetchUsers(@CurrentUser() user: IUser): Promise<IUser[]> {
+        console.log(user)
+        return this.usersService.findAll();
+    }
+
+    // Create Betail
+
+    // fetch Betails
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '~/auth/auth.module';
 import { userModelName } from './schemas/user.model-name';
 import { userSchema } from './schemas/users.schema';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([ { schema: userSchema, name: userModelName } ])],
+  imports: [
+    MongooseModule.forFeature([ { schema: userSchema, name: userModelName } ]),
+    forwardRef(() => AuthModule)
+  ],
   providers: [UsersService],
-  exports: [UsersService]
+  exports: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
